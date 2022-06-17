dd4a8c5088ac3e785a3a2c671d9221db10b1bbbe
Krzysztof2 <sarapuk2017@wp.pl>
2022-06-17 13:15:36 +0200

    random yellow cars parking on the streets

--- Speedster/Assets/scripts/GridSystem.cs
+++ Speedster/Assets/scripts/GridSystem.cs
@@ -11,6 +11,7 @@ public class GridSystem : MonoBehaviour
     public GameObject Home1;
     public GameObject Home2;
     public GameObject Home3;
+    public GameObject yellowCar;
     public int width = 140;
     public int depth = 140;
 
@@ -26,11 +27,16 @@ public class GridSystem : MonoBehaviour
                 Vector3 position = new Vector3(x, 0, z); Vector3 homeposition = new Vector3( x + 35, 0, z + 35);
                 GameObject instatedCrossroad = Instantiate(crossroad, position, Quaternion.identity);
 
+                int randomYellowPosition = Random.Range(15, 55);
+                Vector3 YellowCarPosition = new Vector3(x + 5, 1, z + randomYellowPosition);
+                GameObject instatedYellowCar = Instantiate(yellowCar, YellowCarPosition, Quaternion.identity);
+
                 // trees x = right, z = forward position ( between 16-50) Random.Range(12, 50)
                 //Vector3 treePosition02 = new Vector3(x + 12, 0, z + Random.Range(12, 50))
-                int randomtree = Random.Range(12, 50);
 
-                int randomHouse = Random.Range(1, 4); //print(randomHouse);
+                int randomtree = Random.Range(12, 50); // random tree position
+
+                int randomHouse = Random.Range(1, 4); //instation choose from random houses: Home1, Home2, or Home3
                 if (randomHouse == 1)
                 {
                     GameObject instatedHome = Instantiate(Home1, homeposition, Quaternion.identity); print("Home1");
@@ -48,10 +54,13 @@ public class GridSystem : MonoBehaviour
                 GameObject tree01R = Instantiate(tree01, tree01PositionRight, Quaternion.identity);
 
                 Vector3 tree01PositionLeft = new Vector3(x - 12, 0, z + randomtree);
-                GameObject tree02L = Instantiate(tree01, tree01PositionLeft, Quaternion.identity);
+                GameObject tree01L = Instantiate(tree01, tree01PositionLeft, Quaternion.identity);
 
                 Vector3 tree02PositionRight = new Vector3(x + 12, 0, z + randomtree + 10);
                 GameObject tree02R = Instantiate(tree02, tree02PositionRight, Quaternion.identity);
+
+                Vector3 tree02PositionLeft = new Vector3(x - 12, 0, z + randomtree + 10);
+                GameObject tree02L = Instantiate(tree02, tree02PositionLeft, Quaternion.identity);
             }
         }
     }
