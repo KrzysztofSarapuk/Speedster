602dd5bd42f592cd1e76395f3ca0b52cc556082d
Krzysztof <sarapuk2017@wp.pl>
2022-06-18 07:14:47 +0200

    Yellow car driving

---
+++ Speedster/Assets/scripts/yellowCarMoving.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yellowCarMoving : MonoBehaviour {
+    // Start is called before the first frame update
+
+    public float speed;
+    public Transform yellowCar;
+    public float startPosition;
+    public float endPosition;
+    //private NPC controller;
+
+    void Start () {
+        yellowCar = GetComponent<Transform> ();
+    }
+
+
+    void Update () {
+            if (transform.localPosition.z < startPosition) {
+                transform.Translate (0, 0, speed * Time.deltaTime * -1);
+                if (transform.localPosition.z < endPosition) {
+                transform.Translate (0, 0, (startPosition - endPosition));
+            }
+            }
+        }
+
+    }
