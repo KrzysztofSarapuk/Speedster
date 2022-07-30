ca5e430f927fe18da157df686022f0c9f34229e7
Krzysztof2 <sarapuk2017@wp.pl>
2022-07-30 19:22:48 +0200

    more people on street

--- Speedster/Assets/scripts/GridSystem.cs
+++ Speedster/Assets/scripts/GridSystem.cs
@@ -7,6 +7,8 @@ public class GridSystem : MonoBehaviour
     public GameObject yellowCar; public GameObject blueCar; public GameObject orangeCar; public GameObject Car; public GameObject straightline;
     public GameObject YellowCarMoving; public int width = 140; public int depth = 140;public int rndTree03; public int garage;public int countdown;
     public GameObject woman1Moving; public GameObject woman2Moving; public GameObject woman3Moving; public GameObject woman4Moving;
+    public GameObject woman5Moving; public GameObject woman6Moving; public GameObject woman7Moving; public GameObject woman8Moving;
+    public GameObject woman9Moving; public GameObject woman10Moving; public GameObject woman11Moving; public GameObject woman12Moving;
 
     void Start()
     {
@@ -55,6 +57,37 @@ public class GridSystem : MonoBehaviour
                     Vector3 woma4MovingPosition = new Vector3(7.5f, 0.25f, z + woman4);
                     GameObject instatedWoman4 = Instantiate(woman4Moving, woma4MovingPosition, Quaternion.Euler(0, 180, 0));
 
+                    int woman5 = Random.Range(10, 360); //woman5
+                    Vector3 woma5MovingPosition = new Vector3(109, 0.25f, z + woman5);
+                    GameObject instatedWoman5 = Instantiate(woman5Moving, woma5MovingPosition, Quaternion.identity);
+
+                    int woman6 = Random.Range(10, 360); //woman6
+                    Vector3 woma6MovingPosition = new Vector3(92.5f, 0.25f, z + woman6);
+                    GameObject instatedWoman6 = Instantiate(woman6Moving, woma6MovingPosition, Quaternion.identity);
+
+                    int woman7 = Random.Range(10, 360); //woman7
+                    Vector3 woma7MovingPosition = new Vector3(91f, 0.25f, z + woman7);
+                    GameObject instatedWoman7 = Instantiate(woman7Moving, woma7MovingPosition, Quaternion.Euler(0, 180, 0));
+
+                    int woman8 = Random.Range(10, 360); //woman8
+                    Vector3 woma8MovingPosition = new Vector3(107.5f, 0.25f, z + woman8);
+                    GameObject instatedWoman8 = Instantiate(woman8Moving, woma8MovingPosition, Quaternion.Euler(0, 180, 0));
+
+                    int woman9 = Random.Range(10, 360); //woman9
+                    Vector3 woma9MovingPosition = new Vector3(209, 0.25f, z + woman9);
+                    GameObject instatedWoman9 = Instantiate(woman9Moving, woma9MovingPosition, Quaternion.identity);
+
+                    int woman10 = Random.Range(10, 360); //woman10
+                    Vector3 woma10MovingPosition = new Vector3(192.5f, 0.25f, z + woman10);
+                    GameObject instatedWoman10 = Instantiate(woman10Moving, woma10MovingPosition, Quaternion.identity);
+
+                    int woman11 = Random.Range(10, 360); //woman11
+                    Vector3 woma11MovingPosition = new Vector3(191f, 0.25f, z + woman11);
+                    GameObject instatedWoman11 = Instantiate(woman11Moving, woma11MovingPosition, Quaternion.Euler(0, 180, 0));
+
+                    int woman12 = Random.Range(10, 360); //woman12
+                    Vector3 woma12MovingPosition = new Vector3(207.5f, 0.25f, z + woman12);
+                    GameObject instatedWoman12 = Instantiate(woman12Moving, woma12MovingPosition, Quaternion.Euler(0, 180, 0));
 
 
                     int randomCar1 = Random.Range(1, 4);
